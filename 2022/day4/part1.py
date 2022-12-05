9e9cf9262ec58ee7564bde0e5fc305652ffd1080
Andersw88 <anders.wikstrom.88@gmail.com>
2022-12-05 20:07:18 +0100

    2022 day4

---
+++ 2022/day4/part1.py
@@ -0,0 +1,23 @@
+import numpy as np
+input = np.genfromtxt('input.csv', delimiter='\n', dtype='unicode')
+
+itemCountSet = set()
+
+
+def fillSet(rangeString):
+    start, end = (int(x) for x in rangeString.split('-'))
+
+    section = set()
+    section.update(range(start, end + 1))
+    return section
+
+
+count = 0
+for row in input:
+    first, second = row.split(',')
+    firstSection = fillSet(first)
+    secondSection = fillSet(second)
+    if (firstSection.issubset(secondSection) or firstSection.issuperset(secondSection)):
+        count += 1
+
+print(count)
