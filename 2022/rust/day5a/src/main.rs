340eca7820aa5ee3b7c74cd91101fdc740ec41de
Anders <anders.wikstrom.88@gmail.com>
2023-11-08 16:24:44 +0100

    rust day5

---
+++ 2022/rust/day5a/src/main.rs
@@ -0,0 +1,55 @@
+use std::collections::VecDeque;
+use itertools::Itertools;
+
+
+fn main() {
+    println!();
+    println!();
+
+    
+
+    let rows = include_str!("input.csv")
+        .split('\n');
+    let stack_rows = rows.clone().take_while(|row| {
+        !row.starts_with(" 1")
+    });
+
+    let mut stacks: Vec::<VecDeque::<char>> = vec![VecDeque::from([]);stack_rows.clone().map(|row| row.len()).max().unwrap() / 4 + 1];
+    
+    stack_rows.clone().for_each(|row| {
+        row.chars()
+        .skip(1)
+        .step_by(4)
+        .enumerate()
+        .filter(|(_, b)| *b != ' ')
+        .for_each(|(index, block )| {
+            stacks[index].push_front(block);
+        })
+    });
+
+    let move_rows = rows.skip(stack_rows.count() + 2);
+
+    move_rows.for_each(|row| 
+    {
+
+        let (amount,from,to) = row
+            .splitn(6,' ')
+            .filter_map(|s| s.parse::<usize>().ok())
+            .collect_tuple()
+            .unwrap();
+
+        for _ in 0..amount
+        {
+            let value = stacks[from - 1].pop_back().unwrap();
+            stacks[to - 1].push_back(value);
+        }
+    });
+
+    for stack in stacks.clone()
+    {
+        println!("{:?}", stack);
+    }
+
+    print!("{:?}", stacks.iter().filter_map(|stack| stack.back()).join(""));
+
+}
