ba7c98f225a09776cd24080b91ffb718abda5e9a
Anders <anders.wikstrom.88@gmail.com>
2023-12-05 11:18:18 +0100

    2023 rust day4

---
+++ 2023/rust/day4b/src/main.rs
@@ -0,0 +1,72 @@
+use std::collections::HashSet;
+
+struct Card {
+    copies: i64,
+    winning_numbers: HashSet<u32>,
+    your_numbers: HashSet<u32>,
+}
+
+fn parse_card(line: &str) -> Card {
+    let parts: Vec<&str> = line.split(": ").collect();
+    let card_number: u32 = parts[0].split_whitespace().nth(1).unwrap().parse().unwrap();
+
+    let numbers: Vec<&str> = parts[1].split(" | ").collect();
+    let winning_numbers: HashSet<u32> = numbers[0]
+        .split_whitespace()
+        .map(|num| num.parse().unwrap())
+        .collect();
+    let your_numbers: HashSet<u32> = numbers[1]
+        .split_whitespace()
+        .map(|num| num.parse().unwrap())
+        .collect();
+    assert!(your_numbers.len() == 25 || your_numbers.len() == 8);
+
+    Card {
+        copies: 1,
+        winning_numbers,
+        your_numbers,
+    }
+}
+
+fn main() {
+    println!();
+
+    // let input = include_str!("example.txt");
+    let input = include_str!("input.txt");
+
+    let mut cards: Vec<Card> = vec![];
+
+    for info in input.lines() {
+        let card = parse_card(info);
+        cards.push(card);
+    }
+
+    (0..cards.len())
+        .for_each(|i| {
+            let card = &cards[i];
+            let card_copies = card.copies;
+            let wins = card
+                .your_numbers
+                .intersection(&card.winning_numbers)
+                .count();
+
+            (i + 1..=i + wins).for_each(|j| {
+                if j < cards.len()
+                {
+                    cards[j].copies += card_copies
+                }
+            });
+        });
+
+    for card in &cards {
+        println!(
+            "Card copies {}: Winning Numbers: {:?}, Your Numbers: {:?}",
+            card.copies, card.winning_numbers, card.your_numbers
+        );
+    }
+
+    let result: i64 = cards.iter().map(|card| card.copies).sum();
+
+    println!("{:?}", result);
+
+}
